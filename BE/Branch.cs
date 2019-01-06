07bcba9496a7dc805400c067da61bb02498e984d
26shadar <46422637+26shadar@users.noreply.github.com>
2019-01-06 18:41:12 +0200

    Add files via upload

---
+++ BE/Branch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Trainee
+    {
+        public int ID { get;  set; } 
+        public string PrivateName { get; set; }  
+        public string Family { get; set; }  
+        public string Gender { get; set; }  
+        public string PhoneNumber{ get; set; } 
+        public string Address { get; set; } 
+        public string YearsOfBirth{ get; set; }    
+        public string CarType { get; set; }
+        public string GearType { get; set; }
+        public string SchoolName { get; set; }
+        public string TeachName { get; set; }
+        public string NumLessons { get; set; } 
+        public override string ToString()
+        {
+            return branchNumber + " " + branchName;
+        }
+    }
+}
---
+++ BE/Tester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Tester
+    {
+        public string  IDT { get; set; }
+        public string Family { get; set; }
+        public string PrivateName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int YearOfExperience { get; set; }
+        public int MaxTestForWeek { get; set; }
+        public string CarType { get; set; }
+        public string Gender { get; set; }
+        מטריצה בוליאנית .
+             public int MaxD { get; set; }
+         public override string ToString()
+
+        
+    }
+}
